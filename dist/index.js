ad15c34cebe15f1aa4a39b6dfc498214dcbf06bc
Dmitri Larionov <LarionovDmitri@gmail.com>
2020-01-09 16:56:51 -0600

    added rest of server side code - todo add front end with dist that calls this api

--- dist/index.js
+++ dist/index.js
@@ -22,7 +22,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2bea1f787514eca59ac9";
+/******/ 	var hotCurrentHash = "c03829c044c07d01b401";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -793,9 +793,10 @@ eval("/* WEBPACK VAR INJECTION */(function(__resourceQuery) {/*\n\tMIT License h
   !*** ./src/index.ts ***!
   \**********************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/ts-loader/index.js):\\nError: Cannot find module 'typescript'\\nRequire stack:\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\ts-loader\\\\dist\\\\utils.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\ts-loader\\\\dist\\\\after-compile.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\ts-loader\\\\dist\\\\instances.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\ts-loader\\\\dist\\\\index.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\ts-loader\\\\index.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\loader-runner\\\\lib\\\\loadLoader.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\webpack\\\\lib\\\\NormalModuleFactory.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\webpack\\\\lib\\\\Compiler.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\webpack\\\\lib\\\\webpack.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\webpack-cli\\\\bin\\\\utils\\\\validate-options.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\webpack-cli\\\\bin\\\\utils\\\\convert-argv.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\webpack-cli\\\\bin\\\\cli.js\\n- C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\webpack\\\\bin\\\\webpack.js\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:794:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:687:27)\\n    at Module.require (internal/modules/cjs/loader.js:849:19)\\n    at require (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:161:20)\\n    at Object.<anonymous> (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\ts-loader\\\\dist\\\\utils.js:6:20)\\n    at Module._compile (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:973:10)\\n    at Module.load (internal/modules/cjs/loader.js:812:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:724:14)\\n    at Module.require (internal/modules/cjs/loader.js:849:19)\\n    at require (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:161:20)\\n    at Object.<anonymous> (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\ts-loader\\\\dist\\\\after-compile.js:6:17)\\n    at Module._compile (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:973:10)\\n    at Module.load (internal/modules/cjs/loader.js:812:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:724:14)\\n    at Module.require (internal/modules/cjs/loader.js:849:19)\\n    at require (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:161:20)\\n    at Object.<anonymous> (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\ts-loader\\\\dist\\\\instances.js:6:25)\\n    at Module._compile (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:973:10)\\n    at Module.load (internal/modules/cjs/loader.js:812:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:724:14)\\n    at Module.require (internal/modules/cjs/loader.js:849:19)\\n    at require (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:161:20)\\n    at Object.<anonymous> (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\ts-loader\\\\dist\\\\index.js:5:21)\\n    at Module._compile (C:\\\\Dev\\\\Research\\\\Typescript\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:973:10)\\n    at Module.load (internal/modules/cjs/loader.js:812:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:724:14)\");\n\n//# sourceURL=webpack:///./src/index.ts?");
+"use strict";
+eval("\r\n/**\r\n * Required External Modules\r\n */\r\nvar __importStar = (this && this.__importStar) || function (mod) {\r\n    if (mod && mod.__esModule) return mod;\r\n    var result = {};\r\n    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];\r\n    result[\"default\"] = mod;\r\n    return result;\r\n};\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", { value: true });\r\nvar dotenv = __importStar(__webpack_require__(/*! dotenv */ \"dotenv\"));\r\nvar express_1 = __importDefault(__webpack_require__(!(function webpackMissingModule() { var e = new Error(\"Cannot find module 'express'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())));\r\nvar cors_1 = __importDefault(__webpack_require__(/*! cors */ \"cors\"));\r\nvar helmet_1 = __importDefault(__webpack_require__(/*! helmet */ \"helmet\"));\r\ndotenv.config();\r\n/**\r\n * App Variables\r\n */\r\nif (!process.env.PORT) {\r\n    process.exit(1);\r\n}\r\nvar PORT = parseInt(process.env.PORT, 10);\r\nvar app = express_1.default();\r\n/**\r\n *  App Configuration\r\n */\r\napp.use(helmet_1.default());\r\napp.use(cors_1.default());\r\napp.use(express_1.default.json());\r\n/**\r\n * Server Activation\r\n */\r\nvar server = app.listen(PORT, function () {\r\n    console.log(\"Listening on port \" + PORT);\r\n});\r\nif (true) {\r\n    module.hot.accept();\r\n    module.hot.dispose(function () { return server.close(); });\r\n}\r\n\n\n//# sourceURL=webpack:///./src/index.ts?");
 
 /***/ }),
 
@@ -807,6 +808,39 @@ eval("throw new Error(\"Module build failed (from ./node_modules/ts-loader/index
 /***/ (function(module, exports, __webpack_require__) {
 
 eval("__webpack_require__(/*! webpack/hot/poll?100 */\"./node_modules/webpack/hot/poll.js?100\");\nmodule.exports = __webpack_require__(/*! ./src/index.ts */\"./src/index.ts\");\n\n\n//# sourceURL=webpack:///multi_webpack/hot/poll?");
+
+/***/ }),
+
+/***/ "cors":
+/*!***********************!*\
+  !*** external "cors" ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"cors\");\n\n//# sourceURL=webpack:///external_%22cors%22?");
+
+/***/ }),
+
+/***/ "dotenv":
+/*!*************************!*\
+  !*** external "dotenv" ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"dotenv\");\n\n//# sourceURL=webpack:///external_%22dotenv%22?");
+
+/***/ }),
+
+/***/ "helmet":
+/*!*************************!*\
+  !*** external "helmet" ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"helmet\");\n\n//# sourceURL=webpack:///external_%22helmet%22?");
 
 /***/ })
 
